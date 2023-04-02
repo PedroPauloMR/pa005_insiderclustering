56112666cdfb91ab24e2c1fc3796c81d9be262c8
PedroPauloMR <pedrop.ribeiro@hotmail.com>
2023-04-02 12:46:48 -0300

    atualizacao pa005 parte AWS

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='identificar clientes para um plano de fidelidade para aumentar a receita da empresa',
+    author='Pedro Ribeiro',
+    license='MIT',
+)
